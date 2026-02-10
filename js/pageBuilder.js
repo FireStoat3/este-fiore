670847d49912bd06e645da8089dc6f7f70afaa28
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2026-02-10 18:28:17 +0100

    Fixed possible xss. Removed recursive links. Minor fix to css

--- js/pageBuilder.js
+++ js/pageBuilder.js
@@ -1,19 +1,20 @@
 window.onload = async () => {
     const baseUrl = window.location.origin;
     const areaFile = await recoverAreaFile(baseUrl);
-    buildMenu(areaFile, baseUrl)
+    const currentUrl = window.location.href
 
-    const url = window.location.href
-    if (url.includes("area.html")) {
+    buildMenu(areaFile, baseUrl, currentUrl)
+
+    if (currentUrl.includes("area.html")) {
         const params = new URLSearchParams(window.location.search);
         const areaName = params.get("area")
         buildAreaPage(baseUrl,areaFile,areaName)
-    } else if (url.includes("place.html")) {
+    } else if (currentUrl.includes("place.html")) {
         const placeFile = await recoverPlaceFile(baseUrl)
         const params = new URLSearchParams(window.location.search);
         const areaName = params.get("area")
         const placeName = params.get("place")
-        buildPlacePage(placeFile,areaName,placeName)
+        buildPlacePage(baseUrl,placeFile,areaName,placeName)
     }
 }
 
@@ -31,22 +32,27 @@ const recoverPlaceFile = async (baseUrl) => {
     return file
 }
 
-const buildMenu = async (areaFile, baseUrl) => {
+const buildMenu = async (areaFile, baseUrl, currentUrl) => {
     const keys = Object.keys(areaFile);
+    const areaName = currentUrl.includes("area.html")?new URLSearchParams(window.location.search).get("area"):null;
     let list = ""
     await keys.forEach((key)=> {
-        list+=`<li><a href="${baseUrl}/pages/area.html?area=${key}">${key}</a></li>`
+        if(areaName!=null && currentUrl.includes("place.html")==false && areaName===key) {
+            list+=`<li class="currentPage">${key}</li>`
+        } else {
+            list+=`<li><a href="${baseUrl}/pages/area.html?area=${key}">${key}</a></li>`
+        }
     })
 
     document.body.innerHTML = document.body.innerHTML.replace("{{MENU_LINK}}", list);
 }
 
-const buildPlacePage = async (placeFile,areaName,placeName) => {
+const buildPlacePage = async (baseUrl, placeFile,areaName,placeName) => {
     const place = placeFile[areaName][placeName]
-    const breadcrumbList = `<li>></li><li><a href="./area.html?area=${areaName}">${areaName}</a></li><li>></li><li>${placeName}</li>`
-    document.body.innerHTML = document.body.innerHTML.replace("{{BREADCRUMB_ITEMS}}", breadcrumbList);
-    document.body.innerHTML = document.body.innerHTML.replace("{{PLACE_TITLE}}", placeName);
     if (place) {
+        const breadcrumbList = `<li>></li><li><a href="./area.html?area=${areaName}">${areaName}</a></li><li>></li><li>${placeName}</li>`
+        document.body.innerHTML = document.body.innerHTML.replace("{{BREADCRUMB_ITEMS}}", breadcrumbList);
+        document.body.innerHTML = document.body.innerHTML.replace("{{PLACE_TITLE}}", placeName);
         const sections = Object.keys(place)
         let placePage = ""
         sections.forEach((sectionName) => {
@@ -54,7 +60,7 @@ const buildPlacePage = async (placeFile,areaName,placeName) => {
             const sectionInfo = place[sectionName]
             const image = sectionInfo.image
             if(image) {
-                placePage+= `<img src="${image}">`
+                placePage+= `<img src="${baseUrl}/images/${image}">`
             }
             placePage += `<p>${sectionInfo.description}</p>`
         })
@@ -66,14 +72,13 @@ const buildPlacePage = async (placeFile,areaName,placeName) => {
 
 const buildAreaPage = async (baseUrl,areaFile,areaName) => {
     const area = areaFile[areaName]
-    const breadcrumbList = `<li>></li><li>${areaName}</li>`
-    document.body.innerHTML = document.body.innerHTML.replace("{{BREADCRUMB_ITEMS}}", breadcrumbList);
     if (area) {
+        const breadcrumbList = `<li>></li><li>${areaName}</li>`
+        document.body.innerHTML = document.body.innerHTML.replace("{{BREADCRUMB_ITEMS}}", breadcrumbList);
         document.body.innerHTML = document.body.innerHTML.replace("{{AREA_TITLE}}", areaName);
         document.body.innerHTML = document.body.innerHTML.replace("{{AREA_IMAGE}}", area["image"]);
         document.body.innerHTML = document.body.innerHTML.replace("{{AREA_DESCRIPTION}}", area["description"]);
         const areaPlaces = Object.keys(area["places"])
-        console.log(areaPlaces)
         let placeList = ""
         let i = 1
         await areaPlaces.forEach((placeName) => {
